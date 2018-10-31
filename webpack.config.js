d4d6e681f20470e8957fb7dbe26767bd5bcf6851
lydstyl <lydstyl@gmail.com>
2018-10-31 20:22:59 +0100

    webpack sass with import ok but in style tag

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,24 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
+      {
+        test: /\.sass$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
   }
 };
