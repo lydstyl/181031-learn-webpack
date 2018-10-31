15adfc3c37cfd404602508c40fb29e4f9d5f1a8f
lydstyl <lydstyl@gmail.com>
2018-10-31 22:07:19 +0100

    whatch true is now woking ^^

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import test from './test';
 import './style.sass';
 
 function component() {
-    let element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  let element = document.createElement('div');
 
-  test();
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+  
+document.body.appendChild(component());
+
+test();
